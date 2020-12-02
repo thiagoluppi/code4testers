bd3a6a4afd9eb144f727be2f3880794a2dc164be
Thiago M. Luppi <thiago.luppi@icloud.com>
2020-12-02 19:25:27 -0300

    commitando o metodos

---
+++ metodos.rb
@@ -0,0 +1,17 @@
+def imprime_nome(nome)
+  puts nome
+end
+
+imprime_nome("Thiago")
+
+def soma(valor1, valor2)
+  puts valor1 + valor2
+end
+
+soma(5, 10)
+
+def pega_soma(valor1, valor2)
+  valor1 + valor2
+end
+
+puts pega_soma(3, 5)
