1e2c51212daa84082166a0af6c238da370fb092e
Thiago M. Luppi <thiago.luppi@icloud.com>
2020-12-05 12:50:24 -0300

    commitandoo controles de fluxo

---
+++ carteira.rb
@@ -0,0 +1,10 @@
+nome = "Thiago"
+idade = 5
+
+if (idade >= 18)
+  puts "#{nome}, voce pode tirar carteira de motorista, voce tem #{idade}"
+elsif (idade > 7)
+  puts "#{nome}, voce nao pode tirar carteira de motorista, voce tem #{idade}"
+else
+  puts "#{nome}, voce nao deveria nem estar perguntando isso, voce tem #{idade}"
+end
